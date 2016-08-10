a7ef7ae86f947a9c27aa997d9305904a724b3dc4
James Gilman <jngilman@mac.com>
2016-08-10 11:19:26 -0400

    a lot of changes '

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
 class Location < ActiveRecord::Base
+geocoded_by :full_street_address   
+after_validation :geocode          
+
+  def full_street_address
+  	"#{address}, #{city}, GA"
+  end
+
 end
