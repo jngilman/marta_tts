f7cba44b60740314bc7381a415f68e2bc15d6fd1
James Gilman <jngilman@mac.com>
2016-08-10 09:16:15 -0400

    modified location views and added httparty gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'locations#index'
   resources :locations
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
