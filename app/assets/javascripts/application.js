4e05d6c28436b4d8b2c560ec351f6aa6aaa9df13
James Gilman <jngilman@mac.com>
2016-08-11 11:11:41 -0400

    fixed google api, added & deleted gems, hid key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,29 @@
+$(document).ready(function (){
+ function initialize() {
+   var myLatlng = new google.maps.LatLng(33.784624, -84.422030);
+
+   var mapOptions = {
+     center: { lat: 33.755797, lng: -84.388789},
+     zoom: 8
+   };
+
+   var map = new google.maps.Map(document.getElementById('map-canvas'),
+   mapOptions);
+ }
+
+ google.maps.event.addDomListener(window, 'load,' initialize);
+
+   var mapOptions = {
+    center: { lat: -36.848738, lng: 174.752173},
+    zoom: 15,
+    scrollwheel: false
+  }
+
+   var marker = new google.maps.Marker({
+     position: myLatlng,
+     map: map
+   });
+});
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,5 +38,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
